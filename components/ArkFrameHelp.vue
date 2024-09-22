61014bcd37ba5504c2c7a55535a7c18caa3f0f70
CDXX-LXIX <taocross666@gmail.com>
2024-09-22 20:53:17 +0200

    New transition animations on buttons New display functionality in Budget section

--- components/ArkFrameHelp.vue
+++ components/ArkFrameHelp.vue
@@ -3,7 +3,7 @@
         <div class="mb-4 px-2 sm:px-6 w-full flex flex-row justify-between">
             <p class="text-white pr-4">{{ $t(description) }}</p>
             <button @click="showModal">
-                <HelpButtonIcon class="w-6 h-6 fill-blue-ribbon-700" />
+                <HelpButtonIcon class="w-6 h-6 fill-blue-ribbon-700 hover:fill-white transition delay-100 duration-300 ease-in-out" />
             </button>
         </div>
         <ArkHelpModal :modalid v-model:showmodal="isModal" />
--- components/sections/ArkBudgetCalc.vue
+++ components/sections/ArkBudgetCalc.vue
@@ -3,24 +3,23 @@
         <!-- <ArkFrameHelp description="budget.subtext" :isbutton="false" modalid="budget-help" /> -->
         <div class="flex flex-col gap-2 w-full px-2 sm:px-6 justify-start font-light text-gray-100 text-md">
             <div class="flex flex-row flex-wrap gap-3 w-full justify-start text-lg font-bold">
-                <p class="flex flex-row gap-2 items-center justify-center" :class="totalPulls > 0 ? 'text-blue-ribbon-700' : totalPulls === 0 ? 'text-amber-300' : 'text-red-600'">
+                <p class="flex flex-row gap-2 items-center justify-center"
+                    :class="totalPulls > 0 ? 'text-blue-ribbon-700' : totalPulls === 0 ? 'text-amber-300' : 'text-red-600'">
                     <img :src="recruit_bag" alt="recruitment bag" class="w-6 h-6"> {{ totalPulls }}
                 </p>
             </div>
             <hr>
-            <div v-for="entry, index in budget_entries" :key="entry">
-                <div v-if="budget_entries[index].operator"
-                    class="grid grid-cols-3 w-full justify-between border-b border-gray-700 border-dashed mb-1">
-                    <p>{{ budget_entries[index].operator }}</p>
-                    <p class="text-center">{{ budget_entries[index].banner }}</p>
-                    <p class="text-right">{{ budget_entries[index].pulls }}</p>
-                </div>
+            <div v-for="entry, index in budget_entries" :key="entry" v-show="entry.operator"
+                class="grid grid-cols-3 w-full justify-between border-b border-gray-700 border-dashed mb-1">
+                <p>{{ budget_entries[index].operator }}</p>
+                <p class="text-center">{{ budget_entries[index].banner }}</p>
+                <p class="text-right">{{ budget_entries[index].pulls }}</p>
             </div>
             <hr>
-            <div v-for="entry, index in budget_entries" :key="entry" class="w-full grid grid-cols-10 gap-3">
+            <div v-for="entry, index in budget_entries" :key="entry" class="w-full grid grid-cols-10 gap-3 mb-2 border-b border-white/30 pb-2">
                 <div class="col-span-3">
-                    <label class="block mb-2 text-sm font-medium text-white">{{ $t('budget.operator_field') }}</label>
-                    <input type="text" v-model="budget_entries[index].operator"
+                    <label class="block mb-2 text-sm font-medium text-white">{{ $t('budget.operator_field') }} #{{ index+1 }}</label>
+                    <input type="text" v-model="budget_entries[index].operator" maxlength="30"
                         class="rounded-lg block flex-1 min-w-0 w-full text-sm p-2.5 bg-gray-700 border-gray-600 text-white outline-none">
                 </div>
                 <ArkDropdown class="col-span-3" label="budget.banner_type" :options="banner_types"
@@ -31,11 +30,11 @@
                 <button class="col-span-1 flex justify-center flex-col items-center h-full cursor-default">
                     <p class="block mb-2 text-sm font-medium text-transparent">none</p>
                     <RemoveButtonIcon @click="removeEntry(index)"
-                        class="w-6 h-6 fill-blue-ribbon-700 hover:fill-red-600 ease-in-out delay-75 transition-opacity cursor-pointer" />
+                        class="w-6 h-6 fill-blue-ribbon-700 hover:fill-red-600 cursor-pointer" />
                 </button>
             </div>
             <button
-                class="flex justify-center items-center py-2 mt-2 rounded-lg hover:bg-blue-ribbon-700 fill-blue-ribbon-700 hover:fill-white"
+                class="flex justify-center items-center py-2 rounded-lg transition delay-100 duration-300 ease-in-out hover:bg-blue-ribbon-700 fill-blue-ribbon-700 hover:fill-white"
                 @click="addNewEntry">
                 <AddButtonIcon class="w-6 h-6 " />
             </button>
@@ -51,8 +50,7 @@ const props = defineProps([
 ])
 
 const budget_entries = ref([])
-const orundum_local = ref(props.orundum)
-const permits_local = ref(props.permits)
+
 const banner_types = [
     { name: "budget.banner_standard", value: "∞" },
     { name: "budget.banner_collab", value: "100% on 120" },
